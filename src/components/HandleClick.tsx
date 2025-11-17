e59fb7370a6b403038ec397ab641445e211ed302
ArsenZar <arsenzar@gmail.com>
2025-11-17 17:15:30 +0200

    use event target

--- src/components/HandleClick.tsx
+++ src/components/HandleClick.tsx
@@ -2,8 +2,8 @@
 
 export default function HandleClick() {
 
-    const handleClick = () => {
-        console.log("I'm a button!");
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        console.log(event, "I'm a button!", event.target);
     };
 
     return <button onClick={handleClick}>Click me!</button>;
