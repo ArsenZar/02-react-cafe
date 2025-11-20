35c7b1974f073eeba1a60dc20dfa01112fc1a6c1
ArsenZar <arsenzar@gmail.com>
2025-11-20 15:02:50 +0200

    add css & fist component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
+''
 // src/components/App.tsx
-//import css from "./App.module.css";
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
 
 export default function App() {  
 
   return (
-    <>
-      
-    </>
+    <div className={css.app}>
+      <CafeInfo />
+    </div>
   );
 }
