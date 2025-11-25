6d5973a59d24fed0a61ac6488fd9096b5d6a1266
ArsenZar <arsenzar@gmail.com>
2025-11-25 20:38:36 +0200

    HW 0.3v

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,21 +2,24 @@
 import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
-import { Votes, VoteType } from "../types/votes";
+import type { Votes, VoteType } from "../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
 
 export default function App() {  
 
-  const [votes, setVotes] = useState({
+  const [votes, setVotes] = useState<Votes>({
     good: 0,
     neutral: 0,
     bad: 0
   }
   );
 
-  const handleVote = (type) => {
-
-  }
+  const handleVote = (type: VoteType) => {
+    setVotes({
+      ...votes,
+      [type]: votes[type] + 1,
+    });
+  };
 
   const resetVotes = () => { }
 
