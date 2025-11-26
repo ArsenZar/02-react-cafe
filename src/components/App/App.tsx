72113db6d4a00ca1c388f7ced452f4406edce56c
ArsenZar <arsenzar@gmail.com>
2025-11-26 13:49:18 +0200

    HW 0.4v (step 5)

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,8 @@ import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import type { Votes, VoteType } from "../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 export default function App() {  
 
@@ -27,13 +29,21 @@ export default function App() {
       neutral: 0,
       bad: 0
     });
-   }
+  }
+  
+  const totalVotesCount: number = votes.good + votes.bad + votes.neutral;
+  const positiveRateCount: number = totalVotesCount
+    ? Math.round((votes.good / totalVotesCount) * 100)
+    : 0;
 
 
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={ true } />
+      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      
+
+      
     </div>
   );
 }
